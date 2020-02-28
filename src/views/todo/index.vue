64a30aeece914f62f802c18a522286cb3a89ffc3
vigarbuaa <vigarxueer@126.com>
2020-02-28 15:32:36 +0800

    实现todo页面逻辑，调用list和adadd

--- src/views/todo/index.vue
+++ src/views/todo/index.vue
@@ -2,90 +2,84 @@
   <div class="app-container">
     <!--工具栏-->
     <div class="head-container">
-      <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
-      <crudOperation :permission="permission" />
-      <!--表单组件-->
-      <!--表格渲染-->
-      <el-table
-        ref="table"
-        v-loading="crud.loading"
-        :data="crud.data"
-        size="small"
-        style="width: 100%;"
-        @selection-change="crud.selectionChangeHandler"
-      >
+      <el-input v-model="input" size="medium" placeholder="帅哥,你打算下一步干点what" clearable @change="addTodo" />
+      <el-table ref="table" :data="todos" size="small" style="width: 100%;">
+        <!-- @selection-change="crud.selectionChangeHandler"> -->
         <el-table-column type="selection" width="55" />
         <!-- <el-table-column v-if="columns.visible('id')" prop="id" label="id" /> -->
-        <el-table-column v-if="columns.visible('content')" prop="content" label="内容" />
-        <el-table-column v-permission="['admin','todo:edit','todo:del']" label="操作" width="150px" align="center">
-          <template slot-scope="scope">
-            <udOperation :data="scope.row" :permission="permission" />
-          </template>
-        </el-table-column>
+        <el-table-column prop="content" width="500" label="内容" />
       </el-table>
-      <!--分页组件-->
-      <pagination />
     </div>
   </div>
 </template>
-
 <script>
-import crudTodo from '@/api/todo'
-import CRUD, {
-  presenter,
-  header,
-  form,
-  crud
-} from '@crud/crud'
-  // import rrOperation from '@crud/RR.operation'
-import crudOperation from '@crud/CRUD.operation'
-import udOperation from '@crud/UD.operation'
-import pagination from '@crud/Pagination'
 
-// crud交由presenter持有
-const defaultCrud = CRUD({
-  title: '待办事宜',
-  url: 'api/todo',
-  sort: 'id,desc',
-  crudMethod: { ...crudTodo
-  }
-})
-const defaultForm = {
-  id: null,
-  content: null,
-  completed: null,
-  createBy: null,
-  createDate: null,
-  remarks: null,
-  delFlag: null
-}
+import axios from 'axios'
+import {
+  getToken
+} from '@/utils/auth' // getToken from cookie
+
 export default {
   name: 'Todo',
-  components: {
-    pagination,
-    crudOperation,
-    // rrOperation,
-    udOperation
-  },
-  mixins: [presenter(defaultCrud), header(), form(defaultForm), crud()],
+  components: {},
   data() {
     return {
-      permission: {
-        add: ['admin', 'todo:add'],
-        edit: ['admin', 'todo:edit'],
-        del: ['admin', 'todo:del']
-      },
-      rules: {}
+      rules: {},
+      input: '',
+      todos: []
     }
   },
+  mounted: function() {
+    // this.todos = crud.data
+    var _this = this
+    console.log('call get todo list')
+    const config = {
+      headers: {
+        'Authorization': getToken()
+      }
+    }
+    // this.todos
+    axios.get('http://localhost:8000/api/todo', config)
+      .then(function(response) {
+        // console.log(response)
+        // console.log(response.data.content)
+        _this.todos = response.data.content
+      }).catch(function(error) {
+        console.log(error)
+      })
+  },
   methods: {
-    // 获取数据前设置好接口地址
-    [CRUD.HOOK.beforeRefresh]() {
-      return true
+    addTodo: function() {
+      let newid = 0
+      console.log('add to do triggered!!!' + this.input)
+      const config = {
+        headers: {
+          'Authorization': getToken()
+        }
+      }
+      axios.post('http://localhost:8000/api/todo', {
+        'content': this.input.trim()
+      }, config)
+        .then(function(response) {
+          console.log(response)
+          newid = response.data.id
+        }).catch(function(error) {
+          console.log(error)
+        })
+
+      this.todos.unshift({
+        id: newid,
+        content: this.input.trim(),
+        completed: false
+      })
+      console.log('debug---')
+      this.input = ''
     }
   }
 }
 </script>
 <style scoped>
-
+.head-container {
+  width: 600px
+}
 </style>
