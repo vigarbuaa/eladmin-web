afb3409336e46b9e2d572b5721e5550a45f2a7db
vigarbuaa <vigarxueer@126.com>
2020-02-28 22:53:04 +0800

    todo list 改class
    用table实现效果不太好，此处未将数据直接绑定于table上，而是用el-row进行了循环，整体好控制许多了

--- src/views/todo/index.vue
+++ src/views/todo/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="app-container">
-    <!--工具栏-->
-    <div class="head-container">
-      <el-input v-model="input" size="medium" placeholder="帅哥,你打算下一步干点what" clearable @change="addTodo" />
-      <el-table ref="table" :data="todos" size="small" style="width: 100%;">
-        <!-- @selection-change="crud.selectionChangeHandler"> -->
-        <el-table-column type="selection" width="55" />
-        <!-- <el-table-column v-if="columns.visible('id')" prop="id" label="id" /> -->
-        <el-table-column prop="content" width="500" label="内容" />
-      </el-table>
-    </div>
+    <div class="head-container">To do list Example</div>
+    <el-input v-model="input" size="medium" placeholder="帅哥,你打算下一步干点what" clearable @change="addTodo" />
+    <el-row v-for="(item, index) in todos" :key="item.id" class="list-row">
+      <el-col :xs="2" :sm="1" :md="1" :lg="1" :xl="1" class="check" :class="{ red: !todos[index].completed, 'green': todos[index].completed }">
+        <el-checkbox v-model="item.completed" size="mini" />
+      </el-col>
+      <el-col :xs="20" :sm="22" :md="22" :lg="22" :xl="22">
+        <input v-model="item.content" type="text" class="ipcont" :class="{done: todos[index].completed}">
+      </el-col>
+      <el-col :xs="2" :sm="1" :md="1" :lg="1" :xl="1" class="close">
+        <i class="el-icon-close" @click="del(index)" />
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
@@ -49,6 +52,11 @@ export default {
       })
   },
   methods: {
+    handleSelectionChange: function(selection) {
+      console.log('toggle work--' + selection)
+      console.log(selection)
+      this.styleObj.color = this.styleObj.color === 'red' ? 'green' : 'red'
+    },
     addTodo: function() {
       let newid = 0
       console.log('add to do triggered!!!' + this.input)
@@ -82,4 +90,62 @@ export default {
 .head-container {
   width: 600px
 }
+#app {
+      font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+      color: #756C83;
+    }
+
+    .el-header {
+      text-align: center;
+    }
+
+    .todoinput {
+      margin-bottom: 40px;
+    }
+
+    .list-row {
+      height: 40px;
+      background-color: #fbfbfb;
+      margin-bottom: 5px;
+    }
+
+    .check {
+      text-align: center;
+      line-height: 40px;
+    }
+
+    .red {
+      border-left: #ef5f65 2px solid;
+    }
+
+    .green {
+      border-left: #B9E1DC 2px solid;
+    }
+
+    .ipcont {
+      width: 90%;
+      margin-top: 8px;
+      border: 0;
+      line-height: 24px;
+      background-color: transparent;
+      font-size: 16px;
+      color: #756C83;
+    }
+
+    .close {
+      text-align: center;
+      line-height: 40px;
+    }
+
+    .el-icon-close {
+      cursor: pointer;
+    }
+
+    .el-icon-close:hover {
+      color: #ef5f65;
+    }
+
+    .done {
+      text-decoration: line-through;
+    }
 </style>
